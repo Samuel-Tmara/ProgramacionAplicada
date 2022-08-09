c4644c5634e0d51fdb04ae54d17a996125818bf2
Samuel-Tmara <110635850+Samuel-Tmara@users.noreply.github.com>
2022-08-08 21:15:12 -0500

    Update 07-EJERCICIO01.py

--- CORTE01/07-EJERCICIO01.py
+++ CORTE01/07-EJERCICIO01.py
@@ -1,57 +1,16 @@
-## EJERCICIO 01
-'''
-Los ejercicios son de caracter obligatorio, sin embargo no se les asignará una calificación, el objetivo de estos
-es que ustedes practiquen y mejoren sus habilidades.
-estos los deben subir de manera individual en su cuenta de Git
-'''
-'''
-se les darán multiples funciones (por ahora no importa qué son), así como la documentación correspondiente
- deben completar las tareas marcadas como TODO
-'''
-##
-def f_suma (a,b):
-    '''
-    Esta funcion debe asignar el varor de la suma entre a y b a la variable a
-    :param a: número
-    :param b: número
-    :return: suma entre a y b
-    '''
-    #TODO: En este espacio deben realizar las modificaciones
-
-    return a
-# Se genera un error si no funciona de manera adecuada
-assert f_suma(3,5) == 8
+#PUNTO 1
+def f_suma (a,b): #utilizamos una función definida como "f_suma" y las variables a,b
+ print(a+b) #utilizamos la funcion print para imprimir el mensaje y colocamos la variable a y b con el simbolo de la suma 
+ return(a+b) # se utiliza para establecer el resultado (o valor de retorno) de una función o para llamar la funcion.
+assert f_suma(3,5) == 8 #el assert se utliza como metodo de validacion y asignacion de los valores a las variables 
 assert f_suma(3,-5) == -2
 assert f_suma(-7,-5) == -12
-## modulo
 
-def f_mod(a,b):
-    '''
-    asigna el modulo entre b y a
-    :param a: primer numero
-    :param b: segundo número
-    :return: retorna el módulo entre b y a
-    '''
-    #TODO: En este espacio deben realizar las modificaciones
+#PUNTO 2
 
-    return b
-
-# Se genera un error si no funciona de manera adecuada
-assert f_mod(3,5) == 2
-assert f_mod(3,-5) == -1
-assert f_mod(4,10) == 2
-
-## Cadenas de caracteres
-def f_str_01 (str_01):
-    '''
-    Realice una función que separe por espacio una cadena de caracteres convierte en mayusculas la segunda palabra
-    y retorna la cadena unida sin espacios.
-    NOTA: deben asignar la nueva cadena a la cadena a la variable str_02
-    :param str_01:
-    :return: str_02
-    '''
-    # TODO: En este espacio deben realizar las modificaciones
-    return str_02
-
-assert f_str_01('Hola Mundo') == 'HolaMUNDO'
-assert f_str_01('tengo un dinosaurio') == 'tengoUNdinosaurio'
+def f_mod(a,b): #utilizamos una función definida como "f_mod" y las variables a,b
+    print(b%a) #utilizamos la funcion print para imprimir el mensaje y colocamos la variable a,b invertida puesto que se quiere saber el modulo de B con A el simbolo del modulo "%"
+    return(b%a) #se utiliza para establecer el resultado (o valor de retorno) de una función o para llamar la funcion
+assert f_mod(3,5) #el assert se utliza como metodo de validacion y asignacion de los valores a las variables
+assert f_mod(3,-5)
+assert f_mod(4,10)
