b0a431b2b5aa0aefd84de0a4fc97553893b071b3
Samuel-Tmara <110635850+Samuel-Tmara@users.noreply.github.com>
2022-09-15 21:00:21 -0500

    Update 16-EJERCICO03.py

--- CORTE02/16-EJERCICO03.py
+++ CORTE02/16-EJERCICO03.py
@@ -1,37 +1,62 @@
-## 16-EJERCICO 03 RECURSIÓN
-'''
-Los ejercicios son de caracter obligatorio, sin embargo no se les asignará una calificación, el objetivo de estos
-es que ustedes practiquen y mejoren sus habilidades.
-estos los deben subir de manera individual en su cuenta de Git
-'''
-'''
-se les darán multiples funciones, así como la documentación correspondiente
- deben completar las tareas marcadas como TODO
-'''
-## DEMOSTRACIONES MATEMATICAS
-# ejercicios tomados de: https://elvex.ugr.es/decsai/java/pdf/7D-Ejercicios.pdf
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#TODO:CONEJOS , SECUENCIA DE FIBONACCI
+def fibo(s_num): #se crea la funcion
+    if s_num < 2: #si el numero incial es menor que dos se cumple la condicion y continua
+        return(s_num) #retorna el numero
+    return fibo(s_num-1)+fibo(s_num-2) #dicho numero anteriormente retornado entra en la "formula de la sucecion de fibonnaci" y se retorna
+    if s_num: #simplemente meti el s_num en un bucle para que haga el recorrido la cantidad de veces que quiero los pasos , en este caso serian 13
+        return (s_num) # lo retorna y al cumplir el s_num , que es valor deseado se finaliza y ya
+print(f" la cantidad de pares de crias que pued eponer a la venta el granjero a lo largo de un año es de :{fibo(13)}") #se imprime
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+class vehicle:
+    def _init_(self,d_delivery, cost, model, year, owner):
+        self.d_delivery = d_delivery
+        self.cost = cost
+        self.model = model
+        self.year = year
+        self.owner = owner
+list = []
 
-#TODO: Demuestre por inducción que, para todo n mayor o igual que 4, n!>2^n
+list.append(vehicle('13/09/2022', '20000','Mustang Match 1', '1969', 'Samuel'))
+list.append(vehicle('15/09/2022', '12000','Tesla Model y', '2020', 'negro'))
+list.append(vehicle('06/01/2022', '33000','Porsche Taycan', '2022', 'Paco'))
 
-#TODO: Un granjero ha comprado una pareja de conejos para criarlos y luego venderlos.
-#   Si la pareja de conejos produce una nueva pareja cada mes y la nueva pareja
-#   tarda un mes más en ser también productiva, ¿cuántos pares de conejos podrá
-#   poner a la venta el granjero al cabo de un año?
+for obj in list:
+    print(obj.d_delivery, obj.cost, obj.model, obj.year, obj.owner, sep=' ')  #(sep =' ') es el separador entre los argumentos para la función print()
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-## Ejercicio objetos.
-# TODO: 1. Cree un archivo, Taller donde llevará acabo el codigo principal
-#       crear una lista de vehiculos que se encuentran en el taller en ese momento que tienen asociados: fecha de entrega, costo, modelo, año y dueño
-#       crear una caja donde se almacena el dinero y se lleva registro de los movimientos
-#       crear una lista de empleados que tienen asociado un nombre, cargo, salario, vehiculo y documento
-#       Crear una lista de clientes que tienen asociado un nombre, vehiculo y documento
+class vehicle:
+    def _init_(self, d_delivery, cost, model, year, owner):
+        self.d_delivery = d_delivery
+        self.cost = cost
+        self.model = model
+        self.year = year
+        self.owner = owner
+list_car = []
+list_car.append(vehicle('13/09/2022', '20000','Mustang Match 1', '1969', 'Samuel'))
+list_car.append(vehicle('15/09/2022', '12000','Tesla Model y', '2020', 'negro'))
+list_car.append(vehicle('06/01/2022', '33000','Porsche Taycan', '2022', 'Paco'))
+for obj in list_car:
+    print(obj.d_delivery, obj.cost, obj.model, obj.year, obj.owner, sep=' ')  #(sep =' ') es el separador entre los argumentos para la función print()
 
-# TODO: 2 Ejecute sobre so codigo principal:
-#       ingresar un vehiculo nuevo al taller (validar cupo)
-#       Sacar un vehiculo del taller (se debe generar un pago por parte del cliente)
-#       PAgar a los empleados (se debe hacer una reducción en el dinero de la caja)
-#       Contrarar a una persona nueva
-#       Despedir a una persona
-#       Encontrar un vehiculo a partir de su dueño y determinar si está en el taller
-# TODO: 3 Todas las funciones que implemente deben estar en un script distinto al principal y donde se definen sus clases
-#       NOTA - (la UNICA exepción son las funciones que definen dentro de sus clases.)
+class employee:
+    def _init_(self, name, position, salary, car, document):
+        self.name = name
+        self.position = position
+        self.salary = salary
+        self.car = car
+        self.document = document
+list_emplo = []
+list_emplo.append(employee('calos','secretario','200','Porsche','1003809076' ))
+for obj_e in list_emplo:
+    print(obj_e.name, obj_e.position, obj_e.salary, obj_e.car, obj_e.document, sep=' ')
 
+class client:
+    def _init_(self,name, document, car):
+        self.name=name
+        self.document = document
+        self.car = car
+list_client=[]
+list_client.append(client('Maria','123321','Toyota'))
+for obj_c in list_client:
+    print(obj_c.name, obj_c.document, obj_c.car, sep=' ')
